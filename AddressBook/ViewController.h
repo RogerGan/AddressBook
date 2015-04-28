515faf2fd1d4c082e1b31fd416c782be551469a6
gancj <gancj@gancjdeMac-mini.local>
2015-04-28 16:58:29 +0800

    Initial Commit

---
+++ AddressBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressBook
+//
+//  Created by gancj on 15/4/28.
+//  Copyright (c) 2015年 gancj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddressBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by gancj on 15/4/28.
+//  Copyright (c) 2015年 gancj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
